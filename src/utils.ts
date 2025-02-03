1192612a698e5026b20211d003001a3aa452d2ff
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-02-03 21:33:43 +0200

    fix: modified regex to allow any Unicode letter, digit and whitespace

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,7 @@
 export const normalize = (str: string) => {
     return str
         .toLowerCase()
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .replace(/[^a-zA-Z0-9\s]/g, ' ') // replacing any special char with a space instead of an empty string should avoid game names like "NieR:Automata™" to end up as "NieRAutomata" which will fail in HLTB search
+        .replace(/[^\p{L}\p{N}\p{Zs}]/gu, ' ') // replacing any special char with a space instead of an empty string should avoid game names like "NieR:Automata™" to end up as "NieRAutomata" which will fail in HLTB search
         .replace(/\s\s+/g, ' ') // replacing multiple whitespaces by a single space for consistency
         .trim();
 };
