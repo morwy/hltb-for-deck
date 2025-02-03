58e200afc62ece3a5c932b01348fe25b53f658bd
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-02-03 20:40:56 +0200

    fix: adjusted normalize() tests to be in accordance to latest changes

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -10,13 +10,16 @@ describe('Other functions', () => {
     test('normalize() should remove diacritical marks', () => {
         expect(normalize('Café International')).toBe('cafe international');
     });
+    test('normalize() should allow non-latin characters', () => {
+        expect(normalize('Café International')).toBe('cafe international');
+    });
     test('normalize() should remove very special characters', () => {
         expect(normalize('WORLD END ECONOMiCA episode.01')).toBe(
-            'world end economica episode.01'
+            'world end economica episode 01'
         );
-        expect(normalize('NieR:Automata™')).toBe('nier:automata');
+        expect(normalize('NieR:Automata™')).toBe('nier automata');
         expect(normalize('Never Alone (Kisima Ingitchuna)')).toBe(
-            'never alone (kisima ingitchuna)'
+            'never alone kisima ingitchuna'
         );
     });
 });
