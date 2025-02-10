2d7e2173de76d0b7ad4dbc074085f34af681a31b
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2025-02-10 21:32:10 +0200

    refactor: removed Unicode any whitespace character support

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const normalize = (str: string) => {
     return str
         .toLowerCase()
-        .replace(/[^\p{L}\p{N}\p{Zs}]/gu, ' ') // replacing any special char with a space instead of an empty string should avoid game names like "NieR:Automata™" to end up as "NieRAutomata" which will fail in HLTB search
+        .replace(/[^\p{L}\p{N}]/gu, ' ') // replacing any special char with a space
         .replace(/\s\s+/g, ' ') // replacing multiple whitespaces by a single space for consistency
         .trim();
 };
